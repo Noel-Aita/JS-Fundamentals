b377896741d2b769f3caa6fedb9f6e8b50fa950b
Econi Noel Aita <noelaita03@gmail.com>
2025-05-21 08:39:05 +0300

    added argument for printing first argument

---
+++ 3-value_argument.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+
+// to see if there are no arguments
+if (args[0] === undefined) {
+  console.log("No argument"); // thten Print this if no arguments were passed
+}
+// Check if there is exactly one argument
+else if (args.length === 1) {
+  console.log(args[0]);
+}
