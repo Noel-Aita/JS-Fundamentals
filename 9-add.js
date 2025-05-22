33d9c2f5bd2f3b2a375ea2c0bd128be1bd409360
Econi Noel Aita <noelaita03@gmail.com>
2025-05-22 14:27:48 +0300

    Correctly added the function add to the file 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -3,12 +3,15 @@ const num1 = Number(args[0])
 const num2 = Number(args[1])
 
 if (isNaN(num1) || isNaN(num2)){
-    console.log("Nan")
+    console.log("NaN")
 }
 
 else{
-    const Add = num1 + num2
 
-console.log(Add)
+
+const add = (num1, num2) => num1 + num2
+
+console.log(add(num1, num2))
+
 
 }
