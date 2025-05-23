194dbfdccae5ed8c181c8816e6963f9cee874862
Econi Noel Aita <noelaita03@gmail.com>
2025-05-23 10:39:45 +0300

    added a funtion to check if the argument is a valid positive number to 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,6 +1,9 @@
 const args = process.argv.slice(2);
 const num1 = Number(args[0]) 
-
+if (isNaN(num1) || num1<0){
+    console.log("1")
+}
+else{
 function factorial(n) {
   if (n === 0 || n === 1) {
     return 1; // 0! and 1! are both 1
@@ -11,3 +14,4 @@ function factorial(n) {
 }
 
 console.log(factorial(num1)); 
+}
